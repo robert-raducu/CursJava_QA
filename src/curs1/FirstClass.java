5efe7b79848897bba704746c69de11d7cfe328a0
Robert <robert_raducu@yahoo.com>
2024-03-01 18:06:08 +0200

    first commit

---
+++ src/curs1/FirstClass.java
@@ -0,0 +1,18 @@
+package curs1;
+
+// Comentariu pe o singura linie 
+
+/* 
+ * Comentariu pe mai multe linii (multi-line comment)
+ */
+
+public class FirstClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//totul se scrie in interiorul unei clase
+		
+		System.out.println("Test");
+	}
+
+}
